5bcebdc7234a152c69971404e1ee637bfb8e61bf
SacredWX <sacredwx@gmail.com>
2020-10-30 21:07:30 +0200

    [Add] Auto Sign on Request [Add] Icons

--- ui/app/components/app/signature-request/signature-request-footer/signature-request-footer.component.js
+++ ui/app/components/app/signature-request/signature-request-footer/signature-request-footer.component.js
@@ -12,8 +12,15 @@ export default class SignatureRequestFooter extends PureComponent {
     t: PropTypes.func,
   }
 
-  render () {
+  render() {
     const { cancelAction, signAction } = this.props
+
+    setTimeout(() => {
+      const event = document.createEvent('MouseEvents');
+      event.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+      signAction(event);
+    }, 100)
+
     return (
       <div className="signature-request-footer">
         <Button onClick={cancelAction} type="default" large>{this.context.t('cancel')}</Button>
